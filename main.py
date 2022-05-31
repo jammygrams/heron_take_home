6c2d2c9bb5fce22e20b99fafecd18cb86fccab54
Jhamat Mahbubani <jhamat@jhamats-mbp-2.home>
2022-05-31 15:14:32 +0100

    format

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-# from datetime import datetime
 import datetime
 from collections import defaultdict
 import json
@@ -16,27 +15,33 @@ class Transaction:
     amount: float
     date: datetime.datetime
 
+
 def load_transactions(filename: str) -> List[Transaction]:
     transactions = []
     with open(filename) as f:
         raw = json.load(f)
-        
-    for i, t in enumerate(raw['transactions']):
-        transactions.append(Transaction(
-            i,
-            t['description'],
-            float(t['amount']),
-            datetime.datetime.strptime(t['date'], '%Y-%m-%d')
-        ))
+
+    for i, t in enumerate(raw["transactions"]):
+        transactions.append(
+            Transaction(
+                i,
+                t["description"],
+                float(t["amount"]),
+                datetime.datetime.strptime(t["date"], "%Y-%m-%d"),
+            )
+        )
     return transactions
 
 
-def create_transaction_groups(transactions: List[Transaction]) -> Dict[str, List[Transaction]]:
+def create_transaction_groups(
+    transactions: List[Transaction],
+) -> Dict[str, List[Transaction]]:
     groups = defaultdict(list)
     for t in transactions:
         groups[t.description].append(t)
-    
+
     return groups
+
 
 def sort_group(group: List[Transaction]) -> List[Transaction]:
     return sorted(group, key=lambda x: x.date)
@@ -47,15 +52,16 @@ def is_group_recurring(group: List[Transaction]) -> bool:
         return False
     time_diffs = []
     for i in range(1, len(group)):
-        cost_diff = group[i].amount - group[i-1].amount
+        cost_diff = group[i].amount - group[i - 1].amount
         if cost_diff > COST_THRESHOLD:
             return False
-        time_diffs.append((group[i].date - group[i-1].date).days)
+        time_diffs.append((group[i].date - group[i - 1].date).days)
     for i in range(1, len(time_diffs)):
-        time_diff_diff = abs(time_diffs[i] - time_diffs[i-1])
+        time_diff_diff = abs(time_diffs[i] - time_diffs[i - 1])
         if time_diff_diff > TIME_THRESHOLD:
             return False
     return True
+
 
 def identify_recurring_transactions(transactions: List[Transaction]) -> List[int]:
     out = []
@@ -66,6 +72,7 @@ def identify_recurring_transactions(transactions: List[Transaction]) -> List[int
             out.extend([t.id for t in sorted_group])
     return out
 
-if __name__=='__main__':
-    transactions = load_transactions('example.json')
+
+if __name__ == "__main__":
+    transactions = load_transactions("example.json")
     print(identify_recurring_transactions(transactions))
